10703af9f4ff9b6ef38bf83f2cf57c795db4f1b7
Jose Arturo Guzman <125223327+JoseArturoGuzman@users.noreply.github.com>
2024-06-26 17:52:40 -0400

    El calendario esta creado para que se vea bien visualmente, todavia falta agregarle alguna funcionalidad, pero esta bien por ahora segun mi entender

--- vite-project/src/Paginas/Calendario.jsx
+++ vite-project/src/Paginas/Calendario.jsx
@@ -1,19 +1,26 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import '../Estilos/Calendario.css';
-import 'react-calendar/dist/Calendar.css';
+
+import Sidebar from '../Componentes/Sidebar2.jsx';
+import Header from '../Componentes/Header.jsx';
 
 export function Calendario() {
     const [value, onChange] = useState(new Date());
 
     return (
-        <div className="calendar-container">
-            <h1>Calendario</h1>
-            <Calendar
+        <div>
+            <Header/>
+            <Sidebar/>
+             <div className="calendar-container">
+                 <h1>Calendario</h1>
+                 <Calendar
                 onChange={onChange}
                 value={value}
                 locale="es-ES"
             />
+             </div>
+            <div/>
         </div>
     );
 }
